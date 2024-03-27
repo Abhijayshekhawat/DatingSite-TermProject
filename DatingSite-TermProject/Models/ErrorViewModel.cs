df16f069bf0f19aa1a240d0c71f36e1a3c30ef82
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-03-27 11:27:30 -0400

    Add project files.

---
+++ DatingSite-TermProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatingSite_TermProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
