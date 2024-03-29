b12ea6000717e6746e697742941691fabd953295
Johnson Chieu <tuj64276@temple.edu>
2024-03-29 01:56:46 -0400

    Web API create account

--- DatingSiteCoreAPI/Controllers/DBUserAccountController.cs
+++ DatingSiteCoreAPI/Controllers/DBUserAccountController.cs
@@ -3,18 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 
 using Utilities;
 using System.Data;
-using System.Data
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace DatingSiteCoreAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/CreateAccount")]
     [ApiController]
     public class DBUserAccountController : ControllerBase
     {
 
-
+       
 
     }
+
+
+
+
 
 
 
---
+++ DatingSiteCoreAPI/PrivateUserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingSiteCoreAPI
+{
+    public class PrivateUserInfo
+    {
+
+
+    }
+}
