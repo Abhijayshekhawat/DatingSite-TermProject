20fd74524f390356d1900736d2796312c764661b
Johnson Chieu <tuj64276@temple.edu>
2024-03-28 23:43:24 -0400

    download sqlclient nuget package

---
+++ DatingSiteCoreAPI/Controllers/DBUserAccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Utilities;
+using System.Data;
+using System.Data
+
+namespace DatingSiteCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DBUserAccountController : ControllerBase
+    {
+
+
+
+    }
+
+
+
+
+
+}
