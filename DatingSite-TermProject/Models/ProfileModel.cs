f5010d3a773b968b50f9d72776951c6ec239b226
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-04-03 22:40:52 -0400

    Profile Page design updated.

---
+++ DatingSite-TermProject/Models/ProfileModel.cs
@@ -0,0 +1,30 @@
+﻿namespace DatingSite_TermProject.Models
+{
+    public class ProfileModel
+    {
+        public int ProfileID { get; set; }
+        public int PrivateID { get; set; }
+        public int Age { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string ProfilePhotoURL { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Description { get; set; }
+        public string Occupation { get; set; }
+        public string Interests { get; set; }
+        public string FavouriteCuisine { get; set; }
+        public string FavouriteQuote { get; set; }
+        public string Goals { get; set; }
+        public string CommitmentType { get; set; }
+        public bool IsVisible { get; set; }
+        public string FavouriteMovieGenre { get; set; }
+        public string FavouriteBookGenre { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FavouriteMovie { get; set; }
+        public string FavouriteBook { get; set; }
+        public string FavouriteRestaurant { get; set; }
+        public string Dislikes { get; set; }
+    }
+}
