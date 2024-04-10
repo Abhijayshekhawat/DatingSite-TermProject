618a60c6e8237987daf67ea6595c9264da74433a
Johnson Chieu <tuj64276@temple.edu>
2024-04-10 18:43:47 -0400

    UnitTest

---
+++ DatingSite-TermProjectTests/Controllers/CreateAccountControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DatingSite_TermProject.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingSite_TermProject.Controllers.Tests
+{
+    [TestClass()]
+    public class CreateAccountControllerTests
+    {
+        [TestMethod()]
+        public void CreateAccountTest()
+        {
+
+
+            Assert.Fail();
+        }
+    }
+}
