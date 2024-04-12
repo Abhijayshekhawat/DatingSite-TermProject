640dc751c766a89ff7e3c07609797740d883e2e9
Johnson Chieu <tuj64276@temple.edu>
2024-04-11 20:42:38 -0400

    Make web service for soap and created cards class

---
+++ DatingSiteWebService/Cards.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace DatingSiteWebService
+{
+    public class Cards
+    {
+        private string firstname;
+        private string lastname;
+        private string profilephotoUrl;
+        private string description;
+        private string city;
+        private string state;
+
+
+        public Cards() { }
+        
+
+        public Cards(string firstname, string lastname, string profilephotoUrl, string description, string city, string state)
+        {
+            this.firstname = firstname; 
+            this.lastname = lastname;
+            this.profilephotoUrl = profilephotoUrl;
+            this.description = description;
+            this.city = city;
+            this.state = state;
+
+        }
+        
+
+        public string FirstName
+        {
+            get { return this.firstname; }
+            set { this.firstname = value; }
+
+        }
+        public string LastName
+        {
+            get { return this.lastname; }
+            set { this.lastname = value; }
+
+        }
+
+
+        public string ProfilePhotoURL
+        {
+            get { return this.profilephotoUrl; }
+            set { this.profilephotoUrl = value;}
+        }
+
+        public string Description
+        {
+            get { return this.description; }    
+            set { this.description = value; }
+
+        }
+
+
+        public string City
+        {
+            get { return this.city; }
+            set { this.city = value; }
+
+        }
+
+        public string State
+        {
+            get { return this.state; }
+            set { this.state = value; }
+
+        }
+
+    }
+}
--- DatingSiteWebService/Dashboard.asmx.cs
+++ DatingSiteWebService/Dashboard.asmx.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Utilities;
 
 namespace DatingSiteWebService
 {
@@ -21,6 +25,38 @@ namespace DatingSiteWebService
         public string HelloWorld()
         {
             return "Hello World";
+        }
+
+
+        [WebMethod]
+
+        public List<Cards> ShowAllUsers(int ExcludedPrivateid)
+        {
+            List<Cards> list = new List<Cards>();
+            Cards cards = new Cards();
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetUsersCards";
+
+            SqlParameter inputParameter1 = new SqlParameter("@ExcludedUserId", ExcludedPrivateid);
+            objCommand.Parameters.Add(inputParameter1);
+
+
+            DataSet ds = objDB.GetDataSet(objCommand);
+
+            DataTable dt = ds.Tables[0];
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                 cards = new Cards(dr["FirstName"].ToString(), dr["LastName"].ToString(), dr["ProfilePhotoURL"].ToString(), dr["Description"].ToString(), dr["City"].ToString(), dr["State"].ToString()); 
+            }
+
+            list.Add(cards);
+
+
+            return list;
+
         }
     }
 }
