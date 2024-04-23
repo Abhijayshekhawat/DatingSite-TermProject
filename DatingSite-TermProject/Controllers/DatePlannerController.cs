59427410646a892ffb14eb92f8b28747a0b066d2
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-04-22 22:19:32 -0400

    Added redirect functionality to Date planner

---
+++ DatingSite-TermProject/Controllers/DatePlannerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingSite_TermProject.Controllers
+{
+    public class DatePlannerController : Controller
+    {
+        public IActionResult UpsertDatePlan()
+        {
+            return View("~/Views/Main/Dates/DatePlanner.cshtml");
+        }
+    }
+}
