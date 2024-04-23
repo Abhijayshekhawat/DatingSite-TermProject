52d1a9842a8cdf2bd0dd45c7b8bea7662dcc77f1
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-04-22 22:58:40 -0400

    Added basic show date plan structure

--- DatingSite-TermProject/Controllers/ShowDatePlanController.cs
+++ DatingSite-TermProject/Controllers/ShowDatePlanController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using DatingSiteCoreAPI;
+using DatingSite_TermProject.Models;
+using System.Text.Json;  // needed for JSON serializers
+using System.IO;    // needed for Stream and Stream Reader
+using System.Net;
+using Microsoft.AspNetCore.Http; // need for cookies
+using Utilities;
+using DatingSite_TermProject.Controllers;
 
 namespace DatingSite_TermProject.Controllers
 {
     public class ShowDatePlanController : Controller
     {
-        public IActionResult ShowPlan()
+        public IActionResult ShowDatePlan()
         {
-            return View("~/Views/Dates/ShowDatePlan.cshtml");
+            string savedUsername2 = Request.Cookies["Username"].ToString();
+            UserProfileModel userProfile = new UserProfileModel();
+            int privateid = userProfile.getPrivateId(savedUsername2);
+            ViewBag.ProfileImage = GetUserImage();
+            var datePlan = new DatePlanModel(1, DateTime.Now, TimeSpan.FromHours(19), "Dinner and a movie", "1234 Elm Street, Springfield", 1);
+            return View("~/Views/Main/Dates/ShowDatePlan.cshtml", datePlan);
+        }
+        private String GetUserImage()
+        {
+            string savedUsername = Request.Cookies["Username"].ToString();
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetProfileFromUsername";
+
+            SqlParameter inputParameter1 = new SqlParameter("@Username", savedUsername);
+            objCommand.Parameters.Add(inputParameter1);
+
+
+            DataSet ds = objDB.GetDataSet(objCommand);
+
+            DataTable dt = ds.Tables[0];
+            string picture = "";
+            foreach (DataRow dr in dt.Rows)
+            {
+                picture = dr["ProfilePhotoURL"].ToString();
+                ViewBag.FirstName = dr["FirstName"].ToString();
+            }
+            return picture;
+
         }
     }
 }
