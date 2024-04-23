e2188b5bfba365bda0c9e5799b2d127680677601
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-04-22 22:41:19 -0400

    Added DatePlanModel

---
+++ DatingSite-TermProject/Controllers/ShowDatePlanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingSite_TermProject.Controllers
+{
+    public class ShowDatePlanController : Controller
+    {
+        public IActionResult ShowPlan()
+        {
+            return View("~/Views/Dates/ShowDatePlan.cshtml");
+        }
+    }
+}
---
+++ DatingSite-TermProject/Models/DatePlanModel.cs
@@ -0,0 +1,63 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingSite_TermProject.Models
+{
+    public class DatePlanModel
+    {
+        private int datePlanID;
+        private DateTime date;
+        private TimeSpan time;
+        private string description;
+        private string location;
+        private int dateId;
+        public DatePlanModel()
+        {
+        }
+        public DatePlanModel(int dpID, DateTime dt, TimeSpan t, string desc, string l, int dID)
+        {
+            DatePlanID = dpID;
+            Date = dt;
+            Time = t;
+            Description = desc;
+            Location = l;
+            DateId = dID;
+        }
+
+        public int DatePlanID
+        {
+            get { return datePlanID; }
+            set { datePlanID = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }   
+        public TimeSpan Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public int DateId
+        {
+            get { return dateId; }
+            set { dateId = value; }
+        }
+        public string GetFullDateTime()
+        {            
+            return new DateTime(Date.Year, Date.Month, Date.Day, Time.Hours, Time.Minutes, Time.Seconds).ToString("f");   
+        }
+    }
+}
