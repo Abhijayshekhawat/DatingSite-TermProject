7ce27fa35ca3cfcf1b9eb53c1a08de043206bddd
Abhijayshekhawat <abhijay.shekhawat@gmail.com>
2024-04-24 16:40:26 -0400

    Created chart controller and filter bugfix

---
+++ DatingSite-TermProject/Controllers/ChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatingSite_TermProject.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
