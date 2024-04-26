3a54da3b9580ce57da64aeb2456398e0b916978d
Johnson Chieu <tuj64276@temple.edu>
2024-04-25 22:35:01 -0400

    Created all the APIs / unit testing

---
+++ DatingSiteCoreAPI/Match.cs
@@ -0,0 +1,91 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using Utilities;
+
+namespace DatingSiteCoreAPI
+{
+    public class Match
+    {
+
+        private string matchusername;
+
+
+        public Match() { }
+        
+        public Match(string matchusername)
+        {
+            this.matchusername = matchusername;
+        }
+        public string MatchUsername
+        {
+            get { return this.matchusername; }
+            set { this.matchusername = value; }
+        }
+
+        public List<Card> GetMatches(string savedUsername2)
+        {
+
+
+            List<Card> Cardslist = new List<Card>();
+            Card cards;
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetMatches";
+
+            SqlParameter inputParameter1 = new SqlParameter("@UserName", savedUsername2);
+            objCommand.Parameters.Add(inputParameter1);
+
+
+            DataSet ds = objDB.GetDataSet(objCommand);
+
+            DataTable dt2 = ds.Tables[0];
+
+            foreach (DataRow dr in dt2.Rows)
+            {
+                cards = new Card(
+                    dr["FirstName"].ToString(),
+                    dr["LastName"].ToString(),
+
+                    dr["ProfilePhotoURL"].ToString(),
+                    dr["City"].ToString(),
+                    dr["State"].ToString(),
+                    dr["Tagline"].ToString(),
+                    dr["Occupation"].ToString(),
+                    dr["Interests"].ToString(),
+                    dr["FavouriteCuisine"].ToString(),
+                    dr["FavouriteQuote"].ToString(),
+                    dr["Goals"].ToString(),
+                    dr["CommitmentType"].ToString(),
+                    dr["FavouriteMovieGenre"].ToString(),
+                    dr["FavouriteBookGenre"].ToString(),
+                    dr["Address"].ToString(),
+                    dr["PhoneNumber"].ToString(),
+                    dr["FavouriteMovie"].ToString(),
+                    dr["FavouriteBook"].ToString(),
+                    dr["FavouriteRestaurant"].ToString(),
+                    dr["Dislikes"].ToString(),
+                    dr["AdditionalInterests"].ToString(),
+                    dr["Dealbreaker"].ToString(),
+                    dr["Biography"].ToString(),
+                    int.Parse(dr["Age"].ToString()),
+
+                    dr["Height"].ToString(),
+                    dr["Weight"].ToString(),
+                    dr["Image1"].ToString(),
+                    dr["Image2"].ToString(),
+                    dr["Image3"].ToString(),
+                    dr["Image4"].ToString(),
+                    dr["Image5"].ToString(),
+                    int.Parse(dr["PrivateId"].ToString())
+                );
+
+                Cardslist.Add(cards);
+            }
+            return Cardslist;
+        }
+
+
+
+    }
+}
